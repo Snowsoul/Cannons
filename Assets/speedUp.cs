139e4b84855248527c7b3c2e4c0b1e9794d5ac64
Snowsoul <adrian.bindiu@outlook.com>
2015-03-09 13:07:03 +0000

    Unfinished fast / slowdown powerups

---
+++ Assets/speedUp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speedUp : MonoBehaviour {
+
+    float oldGravity = 0f;
+    float oldSpeed = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SimpleProjectile" || other.tag =="FireProjectile" || other.tag =="FreezeProjectile")
+        {
+            GameObject g = other.gameObject;
+            Projectile projScript = g.GetComponent<Projectile>();
+
+            oldGravity = g.GetComponent<Rigidbody2D>().gravityScale;
+            oldSpeed = projScript.maxSpeed;
+
+            projScript.maxSpeed = 0f;
+
+            g.GetComponent<Rigidbody2D>().gravityScale = 0f;
+
+             
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "SimpleProjectile" || other.tag == "FireProjectile" || other.tag == "FreezeProjectile")
+        {
+            GameObject g = other.gameObject;
+            Projectile projScript = g.GetComponent<Projectile>();
+
+            //projScript.maxSpeed = oldSpeed;
+            g.GetComponent<Rigidbody2D>().gravityScale = oldGravity;
+
+
+        }
+    }
+}
