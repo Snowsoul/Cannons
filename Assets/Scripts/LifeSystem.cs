2d1593d3f4c0436c957968f6caaa89ff8a987858
Snowsoul <adrian.bindiu@outlook.com>
2015-03-19 15:06:43 +0000

    Rounded Healthbar

--- Assets/Scripts/LifeSystem.cs
+++ Assets/Scripts/LifeSystem.cs
@@ -5,22 +5,32 @@ using System.Collections;
 public class LifeSystem : MonoBehaviour {
 
 	public int health = 100;
-	public Text player1HP;
-	public Text player2HP;
+	public GameObject player1HP;
+	public GameObject player2HP;
+    private bool decreaseP1 = false;
+    private bool decreaseP2 = false;
+
 	
 
 	void doDamage(object[] obj){
 		int dmg =  (int)obj [0];
 		string player = obj [1].ToString();
 		health -= dmg;
+  
 		if (health < 0)
 						health = 0;
-						
 		if (player == "Player1")
-						player1HP.text = "HP: " + health;
+        {
+            //player1HP.GetComponent<Image>().fillAmount = (float)health / 100;
+            decreaseP1 = true;
+        }			
 				else
-			if (player == "Player2")
-						player2HP.text = "HP: " + health;
+            if (player == "Player2")
+            {
+                //player2HP.GetComponent<Image>().fillAmount = (float)health / 100;
+                decreaseP2 = true;
+            }
+               
 
 		if (health == 0 && player == "Player1")
 						Debug.Log ("Player 2 Wins!!");
@@ -32,12 +42,27 @@ public class LifeSystem : MonoBehaviour {
 
 	}
 	void Start () {
-		player1HP = GameObject.Find ("Player1HP").GetComponent<Text>();
-		player2HP = GameObject.Find ("Player2HP").GetComponent<Text>();
+		player1HP = GameObject.Find ("Player1HP");
+		player2HP = GameObject.Find ("Player2HP");
 	}
 	
 
 	void Update () {
+
+        if (decreaseP1)
+        {
+            if (player1HP.GetComponent<Image>().fillAmount > (float)health / 100)
+                player1HP.GetComponent<Image>().fillAmount -= 0.01f;
+            else
+                decreaseP1 = false;
+        }
+        if (decreaseP2)
+        {
+            if (player2HP.GetComponent<Image>().fillAmount > (float)health / 100)
+                player2HP.GetComponent<Image>().fillAmount -= 0.01f;
+            else
+                decreaseP2 = false;
+        }
 	
 	}
 }
